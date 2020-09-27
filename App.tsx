10781889803522a6790e464e73f3948ecccc479c
Nitorclik <reimon1972@gamil.com>
2020-09-27 11:55:47 +0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Text} from 'react-native-svg';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <Text>Hello</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
